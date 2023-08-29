c4664f12d3c3895db244464d583b917063b6e499
Nandhini A <Nandy123@>
2023-08-29 22:52:34 +0530

    stop-watch-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import Header from "./components/Header"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+function App(){
+
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  
+  useEffect(() => {
+    let intervalId;
+    //The code we want to run
+    if (isRunning) { 
+      intervalId = setInterval(() => setTime(time + 1), 10);
+    }
+    return () => clearInterval(intervalId);
+  }, 
+  //The dependancy array
+  [isRunning, time]);
+
+  function startAndStop() {
+    setIsRunning(!isRunning);
+  };
+
+  function reset(){
+    setTime(0);
+  }
+
+  const hours = Math.floor(time / 360000);
+  const minutes = Math.floor((time % 360000) / 6000);
+  const seconds = Math.floor((time % 6000) / 100);
+
+  return (<div className="container">
+  <Header/>
+   <h2 >
+     {hours}:{minutes.toString().padStart(2, "0")}:
+     {seconds.toString().padStart(2, "0")}
+   </h2>
+   <div>
+     <button onClick={startAndStop}>{isRunning ? "Stop" : "Start"}</button>
+     
+     <button  onClick={reset}>
+       Reset
+     </button>
+   </div>
+ </div>
+);
+};
 
 export default App;
