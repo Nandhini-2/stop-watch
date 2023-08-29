5c8f308922e910458f72d1204c878462ffb834e8
Nandhini-2 <68190579+Nandhini-2@users.noreply.github.com>
2023-08-29 17:40:46 +0000

    modified

--- src/App.js
+++ src/App.js
@@ -29,12 +29,14 @@ function App(){
   const hours = Math.floor(time / 360000);
   const minutes = Math.floor((time % 360000) / 6000);
   const seconds = Math.floor((time % 6000) / 100);
+  const milliseconds = time % 100;
 
   return (<div className="container">
   <Header/>
    <h2 >
      {hours}:{minutes.toString().padStart(2, "0")}:
-     {seconds.toString().padStart(2, "0")}
+     {seconds.toString().padStart(2, "0")}:
+     {milliseconds.toString().padStart(2, "0")}
    </h2>
    <div>
      <button onClick={startAndStop}>{isRunning ? "Stop" : "Start"}</button>
